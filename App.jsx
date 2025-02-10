bd9cd5a29fc96f5363c3602d13c401af4bbf4abf
JAYRAJSINH14 <130070854+JAYRAJSINH14@users.noreply.github.com>
2025-02-10 21:49:57 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Navbar from './Components/Navbar/Navbar'
+
+const App = () => {
+  return (
+    <div>
+      <Navbar/>
+    </div>
+  )
+}
+
+export default App
+
+//start=6.00.00
+//end=7.25.00
