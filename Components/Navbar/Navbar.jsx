c9d08e2202ddecd5ad257b170dd69f52c2a38482
JAYRAJSINH14 <130070854+JAYRAJSINH14@users.noreply.github.com>
2025-02-10 21:47:20 +0530

    Add files via upload

---
+++ Components/Navbar/Navbar.jsx
@@ -0,0 +1,44 @@
+import React, { useContext, useRef, useState } from 'react'
+import './Navbar.css'
+import logo from '../Assets/logo2.png'
+import cart_icon from '../Assets/cart_icon1.png'
+import { Link } from 'react-router-dom'
+import { ShopContext } from '../../Context/ShopContext'
+import nav_dropdown from '../Assets/drop_down.jpg'
+
+const Navbar = () => {
+
+    const [menu,setMenu] = useState("shop");
+    const {getTotalCartItems}= useContext(ShopContext);
+    const menuRef = useRef();
+
+    const dropdown_toggle = (e) =>{
+      menuRef.current.classList.toggle('nav-menu-visible');
+      e.target.classList.toggle('open');
+    }
+
+  return (
+    <div className='navbar'>
+        <div className="nav-logo">
+            <img src={logo} alt="" width={75}/>
+            <p>JAY'S MART</p>
+        </div>
+    <img onClick={dropdown_toggle} src={nav_dropdown} alt="" width={80} height={80} />
+    <ul ref={menuRef} className="nav-menu">
+      <li onClick={()=>{setMenu("shop")}}><Link style={{ textDecoration: 'none' }} to='/'>Shop</Link>{menu==="shop"?<hr/>:<></>}</li>
+      <li onClick={()=>{setMenu("mens")}}><Link style={{ textDecoration: 'none' }} to='/mens'>Men</Link>{menu==="mens"?<hr/>:<></>}</li>
+      <li onClick={()=>{setMenu("womens")}}><Link style={{ textDecoration: 'none' }} to='/womens'>Women</Link>{menu==="womens"?<hr/>:<></>}</li>
+      <li onClick={()=>{setMenu("kids")}}><Link style={{ textDecoration: 'none' }} to='/kids'>Kids</Link>{menu==="kids"?<hr/>:<></>}</li>
+    </ul>
+    <div className="nav-login-cart">
+      {localStorage.getItem('auth-token')
+      ?<button onClick={()=>{localStorage.removeItem('auth-token');window.location.replace('/')}}>Logout</button>
+      :<Link to='/login'><button>Login</button></Link>}
+      <Link to='/cart'><img src={cart_icon} alt="" width={50} /></Link>
+      <div className="nav-cart-count">{getTotalCartItems()}</div>
+    </div>
+  </div>
+  )
+}
+
+export default Navbar
